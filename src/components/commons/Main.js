55088c8725fff9b0d5211c1b14b35ca1ac41094d
Cody Brunner <rockchalkwushock@icloud.com>
2018-07-11 10:34:33 -0700

    chore(root): removes src for rewrite to gatsby v2

--- src/components/commons/Main.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-const Main = styled.main`
-  min-height: 100vh;
-  width: 100%;
-`
-
-export default Main
--- src/lib/typography.js
+++
@@ -1,4 +0,0 @@
-import Typography from 'typography'
-import lincolnTheme from 'typography-theme-lincoln'
-
-export default new Typography(lincolnTheme)
